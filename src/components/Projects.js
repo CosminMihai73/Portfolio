1ec40511e571ce18b44ce39acfb73c52cf70610b
CosmiMihai73 <daiacosmin666>
2024-01-14 15:50:22 +0200

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,21 +7,21 @@ function Projects() {
         <div className="projects-container">
             <h2>My Projects</h2>
             <div className="project">
+                <Link to="/notes" className="project-details">
+                    <h3>Notes Application</h3>
+                    <p>A notes application made with PHP/HTML/CSS/Js</p>
+                </Link>
+                <Link to="/notes">
+                    <img src={process.env.PUBLIC_URL +"/PROJECT2.png"} alt="Project 1" />
+                </Link>
+            </div>
+            <div className="project">
                 <Link to="/fishstore" className="project-details">
                     <h3>Fishing Store</h3>
                     <p>An online fishing store made with PHP/HTML/CSS/Js</p>
                 </Link>
                 <Link to="/fishstore">
                     <img src={process.env.PUBLIC_URL +"/Fish.png"} alt="Project 1" />
-                </Link>
-            </div>
-            <div className="project">
-                <Link to="/notes" className="project-details">
-                    <h3>Notes Application</h3>
-                    <p>A notes application made with PHP/HTML/CSS</p>
-                </Link>
-                <Link to="/notes">
-                    <img src={process.env.PUBLIC_URL +"/PROJECT2.png"} alt="Project 1" />
                 </Link>
             </div>
         </div>
