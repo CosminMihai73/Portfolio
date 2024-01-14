45e28c59cd455b7ba03d956d151d7755cce14681
CosmiMihai73 <daiacosmin666>
2024-01-14 14:38:46 +0200

    update

--- src/components/About.js
+++ src/components/About.js
@@ -13,12 +13,12 @@ function About() {
 
             </div>
 
-            <h3>Studies</h3>
+            <h3>About me</h3>
             <p>
                 I'm a passionate third-year student pursuing a degree in Informatics for Economics at Transilvania University in Brasov, Romania. My educational journey has equipped me with a strong basis in various aspects of computer technology and information technology.
             </p>
             <p>
-                I specialize in full-stack development and enjoy the creative process of bringing thoughts to life through code. My skill set includes proficiency in several programming languages, such as React, PHP, HTML, JavaScript, Python, Java, and C#.
+                I specialize in full-stack development and enjoy the creative process of bringing thoughts to life through code. My skill set includes proficiency in several programming languages, such as React, PHP, HTML, JavaScript, Python, Java and C#.
             </p>
             <p>
                 Additionally, I have hands-on experience working with databases, leveraging technologies like SQL Server and MySQL to design and manage records. Throughout my educational and personal projects, I've developed and maintained robust databases to support dynamic web applications.
